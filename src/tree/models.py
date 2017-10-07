63c533af5080289f6bb569cb3ab27248dce60aad
Adrian Bury <kubaadrian2@gmail.com>
2017-10-07 14:46:09 +0200

    add example model to tree app

--- src/tree/models.py
+++ src/tree/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+from machinery.models import Machine
+
+
+class Node(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey(to='self', blank=True, null=True)
+    machinery = models.ManyToManyField(to=Machine, blank=True)
+
+    def __unicode__(self):
+        return self.name
