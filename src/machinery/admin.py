01d2494a9255cfa7da0b53f3572d44a8d2beac69
Adrian Bury <kubaadrian2@gmail.com>
2017-10-21 21:09:34 +0200

    add models to admin page

--- src/machinery/admin.py
+++ src/machinery/admin.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+admin_models = [
+    models.MachineTool,
+    models.MachineToolRequirements,
+    models.MachiningCapability,
+    models.SpindleCapability,
+    models.PositioningCapability,
+    models.RangeOfMotion,
+    models.AxisCapability,
+    models.MeasuringCapability,
+    models.MachineSize
+]
+
+admin.site.register(admin_models)
