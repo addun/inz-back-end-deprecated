47912639fbd8aabe52ae7c15f3e16b351306e643
Adrian Bury <kubaadrian2@gmail.com>
2017-10-25 21:15:22 +0200

    add url to xml in admin panel

--- src/machinery/admin.py
+++ src/machinery/admin.py
@@ -2,13 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.urls import reverse
 
 from . import models
 
 # Register your models here.
 admin_models = [
     models.MachineTool,
-    models.MachineToolRequirements,
     models.MachiningCapability,
     models.SpindleCapability,
     models.PositioningCapability,
@@ -17,5 +17,17 @@ admin_models = [
     models.MeasuringCapability,
     models.MachineSize
 ]
+
+
+@admin.register(models.MachineToolRequirements)
+class MachineToolRequirementsAdmin(admin.ModelAdmin):
+    list_display = ['description', 'get_xml']
+
+    def get_xml(self, obj):
+        link = "%s?format=xml" % reverse('machine-tool-requirements-detail', args=[obj.id])
+        return '<a href="{0}">Get XML</a>'.format(link)
+
+    get_xml.allow_tags = True
+
 
 admin.site.register(admin_models)
--- src/machinery/urls.py
+++ src/machinery/urls.py
@@ -4,5 +4,6 @@ from .views import MachineToolRequirementsList, MachineToolRequirementsDetail
 
 urlpatterns = [
     url(r'^machine-tool-requirements/$', MachineToolRequirementsList.as_view()),
-    url(r'^machine-tool-requirements/(?P<pk>\w+)/$', MachineToolRequirementsDetail.as_view()),
+    url(r'^machine-tool-requirements/(?P<pk>\w+)/$', MachineToolRequirementsDetail.as_view(),
+        name='machine-tool-requirements-detail'),
 ]
