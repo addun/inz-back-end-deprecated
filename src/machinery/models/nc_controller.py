9bf3cf54f58a1f48b0339cc35103f0500451cb68
Adrian Bury <kubaadrian2@gmail.com>
2017-11-09 23:07:21 +0100

    change integer to base from schema

--- src/machinery/models/nc_controller.py
+++ src/machinery/models/nc_controller.py
@@ -16,10 +16,10 @@ class NcController(models.Model):
     minimum_linear_increment = schemas.Measure.length_measure()
     minimum_angle_increment = schemas.Measure.plane_angle_measure()
     maximum_number_of_multi_channel_control = schemas.Measure.count_measure()
-    look_ahead = models.IntegerField(null=True, blank=True)
+    look_ahead = schemas.Base.integer(null=True, blank=True)
     adaptive_control = schemas.SupportResource.text(blank=True, null=True)
     miscellaneous_controller_functions = schemas.SupportResource.text(blank=True, null=True)
-    program_memory_size = models.IntegerField(blank=True, null=True)
+    program_memory_size = schemas.Base.integer(blank=True, null=True)
     time_sampling = schemas.Measure.time_measure(blank=True, null=True)
     clock_frequency = schemas.Measure.count_measure(blank=True, null=True)
 
