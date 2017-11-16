0c9b16c4968a4a207fee5c4a4fb93aafd133358a
Adrian Bury <kubaadrian2@gmail.com>
2017-11-16 12:27:58 +0100

    add machine tool axis models

---
+++ src/machinery/models/machine_tool_axis.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from machinery import schemas
+
+
+class MachineToolAxis(models.Model):
+    axis_name = schemas.SupportResource.label()
+
+
+class LinearAxis(MachineToolAxis):
+    minimum_range_of_motion = schemas.Measure.length()
+    maximum_range_of_motion = schemas.Measure.length()
+    displacement_error = schemas.Measure.length()
+    repeatability_error = schemas.Measure.length()
+    rapid_traverse_feed_rate = schemas.Measure.velocity()
+    minimum_cutting_feed_rate = schemas.Measure.velocity()
+    maximum_cutting_feed_rate = schemas.Measure.velocity()
+    maximum_acceleration = schemas.Measure.velocity(null=True, blank=True)
+    maximum_deceleration = schemas.Measure.velocity(null=True, blank=True)
+    maximum_jerk = schemas.Other.jerk_measure(null=True, blank=True)
