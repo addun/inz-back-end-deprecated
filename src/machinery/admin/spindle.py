92d95e8df880cabfda1cefee561c6356ef302ee2
Adrian Bury <kubaadrian2@gmail.com>
2017-11-16 21:41:35 +0100

    add admin Spindle models

---
+++ src/machinery/admin/spindle.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline, GenericStackedInline
+
+from machinery.models import SpindleRange, Spindle, WorkSpindle, TaperedSpindle, StraightSpindle, ThreadedSpindle
+
+
+class ToolAssemblyInline(GenericTabularInline):
+    model = SpindleRange
+    extra = 0
+
+
+@admin.register(Spindle)
+class SpindleAdmin(admin.ModelAdmin):
+    inlines = [
+        ToolAssemblyInline
+    ]
+
+
+@admin.register(WorkSpindle)
+class WorkSpindleAdmin(admin.ModelAdmin):
+    inlines = [
+        ToolAssemblyInline
+    ]
+
+
+@admin.register(TaperedSpindle)
+class TaperedSpindleAdmin(admin.ModelAdmin):
+    inlines = [
+        ToolAssemblyInline
+    ]
+
+
+@admin.register(StraightSpindle)
+class StraightSpindleAdmin(admin.ModelAdmin):
+    inlines = [
+        ToolAssemblyInline
+    ]
+
+
+@admin.register(ThreadedSpindle)
+class ThreadedSpindleAdmin(admin.ModelAdmin):
+    inlines = [
+        ToolAssemblyInline
+    ]
