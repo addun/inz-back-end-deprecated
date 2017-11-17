02d4e24eee743dcce20b42063134f9ce7ef78667
Adrian Bury <kubaadrian2@gmail.com>
2017-11-17 21:45:23 +0100

    add Device and MeasuringCapability models

--- src/machinery/models/__init__.py
+++ src/machinery/models/__init__.py
@@ -3,6 +3,7 @@ from installation import *
 from machine_tool_axis import *
 from machine_tool_requirement import *
 from nc_controller import *
+from others import *
 from rotary_axis import *
 from spindle import *
 from tool_handling_unit import *
---
+++ src/machinery/models/others.py
@@ -0,0 +1,26 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from machinery import schemas
+
+
+class Device(models.Model):
+    device_id = models.AutoField(primary_key=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    id = schemas.SupportResource.identifier()
+    model_name = schemas.SupportResource.label()
+    serial_number = schemas.SupportResource.identifier()
+    manufacturer = schemas.SupportResource.label()
+    date_manufactured = schemas.DateTime.calendar_date(null=True, blank=True)
+
+
+class MeasuringCapability(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    measuring_accuracy = schemas.SupportResource.text()
+    description = schemas.SupportResource.text(null=True, blank=True)
