c0faa824312d4b1ae2c8734196d2bb2b143a41c7
Adrian Bury <kubaadrian2@gmail.com>
2017-11-18 19:01:24 +0100

    add experiment decorator

--- src/machinery/schemas.py
+++ src/machinery/schemas.py
@@ -1,23 +1,6 @@
-import functools
-import warnings
-
 from django.db import models
 
-
-def deprecated(func):
-    """This is a decorator which can be used to mark functions
-    as deprecated. It will result in a warning being emmitted
-    when the function is used."""
-
-    @functools.wraps(func)
-    def new_func(*args, **kwargs):
-        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
-        warnings.warn("Call to deprecated function {}.".format(func.__name__), category=DeprecationWarning,
-                      stacklevel=2)
-        warnings.simplefilter('default', DeprecationWarning)  # reset filter
-        return func(*args, **kwargs)
-
-    return new_func
+from machinery.utils import deprecated
 
 
 class Select(models.Model):
