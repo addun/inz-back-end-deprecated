f1c6a4bfea461bcfe6e2204a27fc542d93df7be3
Adrian Bury <kubaadrian2@gmail.com>
2017-11-18 19:30:22 +0100

    refactor Spinlde admin model

--- src/machinery/admin/spindle.py
+++ src/machinery/admin/spindle.py
@@ -1,46 +1,44 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.contenttypes.admin import GenericStackedInline
 
 from machinery.admin.element_capability import ChuckInline
 from machinery.models import SpindleRange, Spindle, WorkSpindle, TaperedSpindle, StraightSpindle, ThreadedSpindle
 
 
-class ToolAssemblyInline(GenericTabularInline):
+class SpindleRangeInline(GenericStackedInline):
     model = SpindleRange
     extra = 0
+    min_num = 1
 
 
 @admin.register(Spindle)
 class SpindleAdmin(admin.ModelAdmin):
     inlines = [
-        ToolAssemblyInline,
+        SpindleRangeInline,
     ]
 
 
 @admin.register(WorkSpindle)
-class WorkSpindleAdmin(admin.ModelAdmin):
-    inlines = [
-        ToolAssemblyInline,
-        ChuckInline
+class WorkSpindleAdmin(SpindleAdmin):
+    inlines = SpindleAdmin.inlines + [
+        ChuckInline,
     ]
+
+
+class ToolSpindleAdmin(SpindleAdmin):
+    pass
 
 
 @admin.register(TaperedSpindle)
-class TaperedSpindleAdmin(admin.ModelAdmin):
-    inlines = [
-        ToolAssemblyInline
-    ]
+class TaperedSpindleAdmin(ToolSpindleAdmin):
+    pass
 
 
 @admin.register(StraightSpindle)
-class StraightSpindleAdmin(admin.ModelAdmin):
-    inlines = [
-        ToolAssemblyInline
-    ]
+class StraightSpindleAdmin(ToolSpindleAdmin):
+    pass
 
 
 @admin.register(ThreadedSpindle)
-class ThreadedSpindleAdmin(admin.ModelAdmin):
-    inlines = [
-        ToolAssemblyInline
-    ]
+class ThreadedSpindleAdmin(ToolSpindleAdmin):
+    pass
