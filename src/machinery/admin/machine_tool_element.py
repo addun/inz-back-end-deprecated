41910da248c8249ffc6d27e623def695ace46a66
Adrian Bury <kubaadrian2@gmail.com>
2017-11-18 21:06:31 +0100

    add inline models to MachineToolElement

--- src/machinery/admin/machine_tool_element.py
+++ src/machinery/admin/machine_tool_element.py
@@ -1,12 +1,60 @@
 from django.contrib import admin
 
-from machinery.models import MachineElementRelationship, MachineToolElement, ElementLinkAssociation
+from machinery.models import MachineElementRelationship, MachineToolElement, ElementLinkAssociation, ToolChanger
+from machinery.models.element_capability import Tailstock, Coolant, BarFeeder, Collet, Chuck
+from machinery.models.machine_tool_axis import MachineToolAxis, LinearAxis
+from machinery.models.rotary_axis import ContinuousRotary, Indexing, LimitedSwing
+from machinery.models.sensor import ToolSetting, PartProbe, ToolBreakage
+from machinery.models.spindle import Spindle, TaperedSpindle, StraightSpindle, ThreadedSpindle, \
+    WorkSpindle
+from machinery.models.tool_handling_unit import ToolMagazine, Turret
+from machinery.models.work_table import RectangularWorkTable, Pallet, CircularWorkTable
+from machinery.utils import experimental
+
+
+def get_inline_by_model(model):
+    return type(
+        'DynamicInline',
+        (admin.StackedInline,),
+        {'model': model, 'extra': 0, 'show_change_link': True}
+    )
 
 
 class MachineToolElementInline(admin.TabularInline):
     model = MachineToolElement
     extra = 0
     show_change_link = True
+
+
+@experimental
+@admin.register(MachineToolElement)
+class MachineToolElementAdmin(admin.ModelAdmin):
+    inlines = [
+        get_inline_by_model(ToolChanger),
+        get_inline_by_model(ToolMagazine),
+        get_inline_by_model(Turret),
+        get_inline_by_model(MachineToolAxis),
+        get_inline_by_model(LinearAxis),
+        get_inline_by_model(ContinuousRotary),
+        get_inline_by_model(Indexing),
+        get_inline_by_model(LimitedSwing),
+        get_inline_by_model(Tailstock),
+        get_inline_by_model(Coolant),
+        get_inline_by_model(BarFeeder),
+        get_inline_by_model(Collet),
+        get_inline_by_model(Spindle),
+        get_inline_by_model(TaperedSpindle),
+        get_inline_by_model(StraightSpindle),
+        get_inline_by_model(ThreadedSpindle),
+        get_inline_by_model(WorkSpindle),
+        get_inline_by_model(Chuck),
+        get_inline_by_model(RectangularWorkTable),
+        get_inline_by_model(Pallet),
+        get_inline_by_model(CircularWorkTable),
+        get_inline_by_model(ToolSetting),
+        get_inline_by_model(PartProbe),
+        get_inline_by_model(ToolBreakage),
+    ]
 
 
 @admin.register(MachineElementRelationship)
@@ -16,9 +64,4 @@ class MachineElementRelationshipAdmin(admin.ModelAdmin):
 
 @admin.register(ElementLinkAssociation)
 class ElementLinkAssociationAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(MachineToolElement)
-class MachineToolElementAdmin(admin.ModelAdmin):
     pass
--- src/machinery/models/machine_tool_element.py
+++ src/machinery/models/machine_tool_element.py
@@ -12,7 +12,7 @@ class MachineToolElement(models.Model):
 
 
 class ElementLinkAssociation(models.Model):
-    machine_tool_element = models.ForeignKey(MachineToolElement, on_delete=models.CASCADE)
+    element = models.ForeignKey(MachineToolElement)
     machine_link = schemas.KinematicStructure.kinematic_link()
 
 
@@ -20,9 +20,7 @@ class MachineElementRelationship(models.Model):
     class_name = schemas.SupportResource.label()
     latter_element = models.OneToOneField(MachineToolElement,
                                           on_delete=models.CASCADE,
-                                          verbose_name='latter_element',
                                           related_name='latter_element_to_MachineToolElement')
     former_element = models.OneToOneField(MachineToolElement,
                                           on_delete=models.CASCADE,
-                                          verbose_name='former_element',
                                           related_name='former_element_to_MachineToolElement')
