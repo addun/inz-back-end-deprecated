5a2c387ac4be7029fe82371f3fde12158d5db8e0
Adrian Bury <kubaadrian2@gmail.com>
2017-11-18 19:20:08 +0100

    fix fields name

--- src/machinery/models/spindle.py
+++ src/machinery/models/spindle.py
@@ -3,7 +3,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from machinery import schemas
-from machinery.models.element_capability import ElementCapability, Chuck
+from machinery.models.element_capability import ElementCapability
 
 
 class Spindle(ElementCapability):
@@ -13,12 +13,15 @@ class Spindle(ElementCapability):
     manufacturer_model_designation = schemas.SupportResource.label(null=True, blank=True)
 
 
-class WorkSpindle(Spindle):
-    spindle_nose_designation = schemas.SupportResource.label()
-    spindle_bore_diameter = schemas.Measure.length()
-    round_bar_stock_diameter = schemas.Measure.length(null=True, blank=True)
-    through_hole_diameter = schemas.Measure.length(null=True, blank=True)
-    hex_bar_stock_capacity = schemas.Base.real(null=True, blank=True)
+class SpindleRange(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    minimum_drive_speed = schemas.Machining.rot_speed()
+    maximum_drive_speed = schemas.Machining.rot_speed()
+    minimum_drive_torque = schemas.Measure.torque()
+    maximum_drive_torque = schemas.Measure.torque()
 
 
 class ToolSpindle(Spindle):
@@ -40,16 +43,13 @@ class StraightSpindle(ToolSpindle):
 
 class ThreadedSpindle(ToolSpindle):
     spindle_thread_diameter = schemas.Measure.length()
-    spindle_thread_pspindle_thread_formitch = schemas.Measure.length()
+    spindle_thread_pitch = schemas.Measure.length()
     spindle_thread_form = schemas.SupportResource.label()
 
 
-class SpindleRange(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey('content_type', 'object_id')
-
-    minimum_drive_speed = schemas.Machining.rot_speed()
-    maximum_drive_speed = schemas.Machining.rot_speed()
-    minimum_drive_torque = schemas.Other.torque_measure()
-    maximum_drive_torque = schemas.Other.torque_measure()
+class WorkSpindle(Spindle):
+    spindle_nose_designation = schemas.SupportResource.label()
+    spindle_bore_diameter = schemas.Measure.length()
+    round_bar_stock_diameter = schemas.Measure.length(null=True, blank=True)
+    through_hole_diameter = schemas.Measure.length(null=True, blank=True)
+    hex_bar_stock_capacity = schemas.Base.real(null=True, blank=True)
