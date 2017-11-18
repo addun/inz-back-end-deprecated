2fec146bbc4b2f2793e1d6176dc50558e1a18ecb
Adrian Bury <kubaadrian2@gmail.com>
2017-11-18 01:15:35 +0100

    add EnvironmentalEvaluation models

---
+++ src/machinery/models/environmental_evaluation.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from machinery import schemas
+
+
+class EnvironmentalEvaluation(models.Model):
+    evaluation_name = schemas.SupportResource.label()
+    power_in_idling = schemas.Measure.power(null=True, blank=True)
+    time_for_warming_up = schemas.Measure.time(null=True, blank=True)
+
+
+class StandardMachiningProcess(models.Model):
+    environmental_evaluation = models.ForeignKey(EnvironmentalEvaluation, on_delete=models.CASCADE)
+    process_description = schemas.SupportResource.text()
+    type_of_machining = schemas.SupportResource.label()
+    power = schemas.Measure.power()
+    electric_power = schemas.Measure.power()
+
+
+class EmissionProperty(models.Model):
+    environmental_evaluation = models.ForeignKey(StandardMachiningProcess, on_delete=models.CASCADE)
+    emission_type = schemas.SupportResource.label()
+    weight = schemas.Measure.mass()
