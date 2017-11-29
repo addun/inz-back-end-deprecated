026ac507e9687b36892d3bd9cb474f0cefb9fca6
Adrian Bury <kubaadrian2@gmail.com>
2017-11-29 16:30:57 +0100

    add class to create dynamic REST based on model

--- src/machinery/urls.py
+++ src/machinery/urls.py
@@ -1,45 +1,44 @@
+from camel_snake_kebab import *
 from django.conf.urls import url, include
 from rest_framework import routers, serializers
 from rest_framework import viewsets
 
-from machinery.models import ToolSetting
-from machinery.views import MachineToolAxisViewSet, LinearAxisViewSet, ContinuousRotaryViewSet, IndexingViewSet, \
-    LimitedSwingViewSet
+from machinery.models import ToolSetting, PartProbe, ToolBreakage
 
 
-def get_view_set(model):
-    return type(
-        'DynamicViewSet', (viewsets.ModelViewSet,),
-        {
-            'queryset': model.objects.all(),
-            'serializer_class': get_serializer(model),
-        }
-    )
+class CustomRouter(routers.DefaultRouter):
+
+    def register_by_model(self, model):
+        router_name = kebab_case(model.__name__)
+        self.register(r'%s' % router_name, self.get_view_set(model))
+
+    def get_view_set(self, model):
+        return type(
+            'DynamicViewSet', (viewsets.ModelViewSet,),
+            {
+                'queryset': model.objects.all(),
+                'serializer_class': self.get_serializer(model),
+            }
+        )
+
+    def get_serializer(self, model):
+        meta = type('Meta', (object,), {
+            'model': model,
+            'fields': '__all__'
+        })
+
+        dynamic_serializer = type(
+            'DynamicSerializer', (serializers.ModelSerializer,), {
+                'Meta': meta
+            }
+        )
+        return dynamic_serializer
 
 
-def get_serializer(model):
-    meta = type('Meta', (object,), {
-        'model': model,
-        'fields': '__all__'
-    })
-
-    dynamic_serializer = type(
-        'DynamicSerializer', (serializers.ModelSerializer,), {
-            'Meta': meta
-        }
-    )
-    return dynamic_serializer
-
-
-router = routers.DefaultRouter()
-router.register(r'machine-tool-axis', MachineToolAxisViewSet)
-router.register(r'linear-axis', LinearAxisViewSet)
-
-router.register(r'continuous-rotary', ContinuousRotaryViewSet)
-router.register(r'indexing', IndexingViewSet)
-router.register(r'limited-swing', LimitedSwingViewSet)
-
-router.register(r'tool-setting', get_view_set(ToolSetting))
+router = CustomRouter()
+router.register_by_model(ToolSetting)
+router.register_by_model(PartProbe)
+router.register_by_model(ToolBreakage)
 
 urlpatterns = [
     url(r'^', include(router.urls))
