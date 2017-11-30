dddf18f6fb4d4f9c4a9030037e71be9eec95d50b
Adrian Bury <kubaadrian2@gmail.com>
2017-11-30 16:35:11 +0100

    add node admin

--- src/tree/admin.py
+++ src/tree/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from tree.models import Node
+
+
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'parent'
+    ]
